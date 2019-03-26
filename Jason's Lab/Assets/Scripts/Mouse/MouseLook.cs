8d021ed890d0913262309ec77c76ee0e677f9215
Jason Sivak <20.jason.sivak@ksd.org>
2019-03-26 13:59:36 -0700

    Yeet

---
+++ Jason's Lab/Assets/Scripts/Mouse/MouseLook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+    float HorizontalSensitivity;
+    float VerticalSensitivity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseLock();
+    }
+
+    void MouseLock()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void MouseInput()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+    }
+}
